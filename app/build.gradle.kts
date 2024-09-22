0b5809ac45db823cf42367cb3aafd93021821b4a
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2024-09-22 17:40:50 +0800

    Update agp and app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "me.velc.devqs"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
