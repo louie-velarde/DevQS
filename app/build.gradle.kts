1f1b014af4b2738a967be9835538b7e6de70bc80
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2024-11-14 20:46:16 +0800

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "me.velc.devqs"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     buildTypes {
