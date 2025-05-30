e31a078016a37d79749e49b55e74ec06b807d0bc
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2025-05-31 00:06:02 +0800

    Update target sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "me.velc.devqs"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "me.velc.devqs"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 3
         versionName = "1.1.1"
     }
