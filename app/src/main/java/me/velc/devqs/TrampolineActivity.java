58e8f5ae718cce5697e18b1bb44bd8475ecd625b
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2026-06-21 19:37:49 +0800

    Show Wi-Fi Debugging screen using Sui (GH-8)

--- app/src/main/java/me/velc/devqs/TrampolineActivity.java
+++ app/src/main/java/me/velc/devqs/TrampolineActivity.java
@@ -1,11 +1,18 @@
 package me.velc.devqs;
 
+import static me.velc.devqs.Utils.execute;
+
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+
+import rikka.shizuku.Shizuku;
+import rikka.sui.Sui;
 
 public class TrampolineActivity extends Activity {
 
@@ -14,7 +21,16 @@ public class TrampolineActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		finish();
 
-		if ("1".equals(Settings.Global.getString(
+		if (showWifiDebuggingScreen(getIntent())) {
+			execute("am",
+			        "start",
+			        "-n",
+			        "com.android.settings/com.android.settings.SubSettings",
+			        "-e",
+			        ":settings:show_fragment",
+			        "com.android.settings.development.WirelessDebuggingFragment");
+
+		} else if ("1".equals(Settings.Global.getString(
 				getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED))) {
 			startActivity(makeIntent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS));
 
@@ -24,19 +40,25 @@ public class TrampolineActivity extends Activity {
 				intent.setData(Uri.parse("package:" + getPackageName()));
 				startActivity(intent);
 				return;
-			} catch (ActivityNotFoundException ignored) {}
+			} catch (ActivityNotFoundException ignored) {
+			}
 
 			try {
 				startActivity(makeIntent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS));
 				return;
-			} catch (ActivityNotFoundException ignored) {}
+			} catch (ActivityNotFoundException ignored) {
+			}
 
 			try {
 				startActivity(makeIntent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS));
 				return;
-			} catch (ActivityNotFoundException ignored) {}
+			} catch (ActivityNotFoundException ignored) {
+			}
 		}
 	}
+
+	private static final String WIRELESS_DEBUGGING =
+			BaseDevQsTileService.WirelessDebugging.class.getName();
 
 	private static Intent makeIntent(String action) {
 		var intent = new Intent(action);
@@ -44,5 +66,13 @@ public class TrampolineActivity extends Activity {
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 		intent.addFlags(Intent.FLAG_ACTIVITY_TASK_ON_HOME);
 		return intent;
+	}
+
+	private static boolean showWifiDebuggingScreen(Intent intent) {
+		ComponentName cName = intent.getParcelableExtra(Intent.EXTRA_COMPONENT_NAME);
+		return cName != null &&
+		       cName.getClassName().equals(WIRELESS_DEBUGGING) &&
+		       Sui.isSui() &&
+		       Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED;
 	}
 }
