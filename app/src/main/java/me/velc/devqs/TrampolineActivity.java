60cb3cd72f1f3fd0d95b73d104fb27ae467e726a
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2026-06-22 10:57:36 +0800

    Update Wi-Fi Debugging screen command

--- app/src/main/java/me/velc/devqs/TrampolineActivity.java
+++ app/src/main/java/me/velc/devqs/TrampolineActivity.java
@@ -2,6 +2,7 @@ package me.velc.devqs;
 
 import static me.velc.devqs.Utils.execute;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
@@ -22,38 +23,49 @@ public class TrampolineActivity extends Activity {
 		finish();
 
 		if (showWifiDebuggingScreen(getIntent())) {
-			execute("am",
-			        "start",
-			        "-n",
-			        "com.android.settings/com.android.settings.SubSettings",
-			        "-e",
-			        ":settings:show_fragment",
-			        "com.android.settings.development.WirelessDebuggingFragment");
+			try {
+				final var pkgName = "com.android.settings";
+				var res = getPackageManager().getResourcesForApplication(pkgName);
 
-		} else if ("1".equals(Settings.Global.getString(
+				@SuppressLint("DiscouragedApi")
+				var titleResId = res.getIdentifier("adb_wireless_settings", "string", pkgName);
+				execute("am", "start",
+				        "-n", pkgName + '/' + pkgName + ".SubSettings",
+				        "--es", ":settings:show_fragment",
+				        pkgName + ".development.WirelessDebuggingFragment",
+				        "--ei", ":settings:show_fragment_title_resid",
+				        Integer.toString(titleResId),
+				        "--activity-clear-task",
+				        "--activity-task-on-home");
+				return;
+			} catch (PackageManager.NameNotFoundException ignored) {
+			}
+		}
+
+		if ("1".equals(Settings.Global.getString(
 				getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED))) {
 			startActivity(makeIntent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS));
+			return;
+		}
 
-		} else {
-			try {
-				var intent = makeIntent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-				intent.setData(Uri.parse("package:" + getPackageName()));
-				startActivity(intent);
-				return;
-			} catch (ActivityNotFoundException ignored) {
-			}
+		try {
+			var intent = makeIntent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+			intent.setData(Uri.parse("package:" + getPackageName()));
+			startActivity(intent);
+			return;
+		} catch (ActivityNotFoundException ignored) {
+		}
 
-			try {
-				startActivity(makeIntent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS));
-				return;
-			} catch (ActivityNotFoundException ignored) {
-			}
+		try {
+			startActivity(makeIntent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS));
+			return;
+		} catch (ActivityNotFoundException ignored) {
+		}
 
-			try {
-				startActivity(makeIntent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS));
-				return;
-			} catch (ActivityNotFoundException ignored) {
-			}
+		try {
+			startActivity(makeIntent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS));
+			return;
+		} catch (ActivityNotFoundException ignored) {
 		}
 	}
 
