997504bd9ca437eb7830fd86f03fe183d15c80c9
Louie Velarde <166181979+louie-velarde@users.noreply.github.com>
2026-07-25 17:03:06 +0800

    Update proguard setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,11 +19,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            optimization {
+                enable = false
+            }
         }
     }
 
